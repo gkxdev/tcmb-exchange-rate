1447b81ff24e217cb464f13c69fcbaa29533a4f8
Gökhan Kaya <gkxdev@gmail.com>
2023-10-29 21:55:19 +0300

    Update example.php

--- example.php
+++ example.php
@@ -3,7 +3,6 @@ require_once __DIR__ . '/TCMB_Exchange_Rate.php';
 
 try {
 
-    // Bugün
     $tcmb = new TCMB_Exchange_Rate;
     // $tcmb = new TCMB_Exchange_Rate('2023-01-01');
 
@@ -14,9 +13,7 @@ try {
     // USD Satış
     echo $tcmb->getCurrency('USD')->selling;
 
-    // Tümü
     print_r($tcmb->getAllCurrencies());
-    // Alınan Tarih
     print_r($tcmb->getDate());
 
 } catch (Exception $e) {
